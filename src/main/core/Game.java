628bf63d53dcf46c3c4f9ab5300cefef949b4bb3
Alexandre Dagnelies <alexandre.dagnelies@koben.fr>
2016-05-13 15:09:30 +0200

    Add user game algo concat

--- src/main/core/Game.java
+++ src/main/core/Game.java
@@ -1,13 +1,51 @@
 package main.core;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import definitions.Concat;
+
 public class Game extends Algo {
 
 	public static void main(String[] args) {
 		
+		ArrayList<String> ascii = new ArrayList<String>();
+		
+		ascii.add(" #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ###  ");
+		ascii.add("# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   #  ");
+		ascii.add("### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ##  ");
+		ascii.add("# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #        ");
+		ascii.add("# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  ");
+		
+		ArrayList<String> coucou = algo(4, 5, "Jessy", ascii);
 		
 		
-		
-
+		for (String string : coucou){
+			System.out.println(string);
+		}
+		Data();
 	}
+	
+	public static ArrayList<String> Data() {
+		ArrayList<String> list = new ArrayList<String>();
+		
+		Random random = new Random();
+	    Boolean randBool = random.nextBoolean();
+	   /*
+	    Concat concat = new Concat();
+	    
+	    ArrayList<String> test = concat.getPattern1();
+	    if (randBool == true){
+	    	//Call pattern1
+	    	
+	    	
+	    }else{
+	    	//Call pattern2
+	    }
+		*/
+		return list;
+	}
+	 
+	
 
 }
--- src/main/core/User.java
+++ src/main/core/User.java
@@ -2,13 +2,19 @@ package main.core;
 
 import java.util.ArrayList;
 
-public class User extends Algo {
+public class User extends Game {
 
 	public static void main(String[] args) {
 		
+		ArrayList<String> ascii = new ArrayList<String>();
 		
+		ascii.add(" #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ###  ");
+		ascii.add("# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   #  ");
+		ascii.add("### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ##  ");
+		ascii.add("# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #        ");
+		ascii.add("# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  ");
 		
-		ArrayList<String> coucou = algo(4, 5, "J");
+		ArrayList<String> coucou = algo(4, 5, "Jessy", ascii);
 		
 		
 		for (String string : coucou){
